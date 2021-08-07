3a5a1c9e14d4e51f76cc0eb412658f302caf2df5
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2021-08-07 14:09:15 +0530

    Addition: Solution to 2nd problem of maths

--- CheatCode_Solutions/2_Maths_Minimum_steps_to_make_product_equal_to_one.cpp
+++ CheatCode_Solutions/2_Maths_Minimum_steps_to_make_product_equal_to_one.cpp
@@ -37,3 +37,62 @@ Constraints:
 1 ≤ N ≤ 10^5
 -10^3 ≤ arr[i] ≤ 10^3
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int MinStep(int a[], int n)
+{
+    // To store the count of 0s, positive
+    // and negative numbers
+    int positive = 0,
+        negative = 0,
+        zero = 0;
+    
+    // To store the number of steps
+    int step = 0;
+    for(int i=0; i<n; i++){
+        // If array element is equal to 0
+        if(a[i] == 0){
+            zero++;
+        }
+
+        // If array element is a negative number
+        else if(a[i] < 0)
+        {
+            negative++;
+
+            // Extra steps needed to make it -1
+            step = step + (-1 - a[i]);
+        }
+
+        // If array element is a positive number
+        else{
+            positive++;
+            // This statement gives us extra steps needed to make it 1
+            step = step + (a[i] - 1);
+        }
+    }
+
+    // At this point array will have only -1s, 0s and 1s
+    if(negative % 2 == 0){
+        // as 
+        step += zero;
+    }
+    else{
+        if(zero>0){
+            step += zero; 
+        }
+        else{
+            step += 2;
+        }
+    }
+}
+
+int main()
+{
+    int a[] = {3,2,5,-2,5,-2};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<< MinStep(a, n);
+
+    return 0;
+}
