5315c29d100ccf4f8b0ce663ec24b9c5e80f162f
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2021-12-12 18:09:38 +0530

    enhancement: added a func tion that uses bit manipulations to check power of two

--- Code_help/07_power_of_two.cpp
+++ Code_help/07_power_of_two.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-// retrns 
+// returns true if number is power of two 
 bool isPowerOfTwo(int n){
     int ans = 1;
     for(int i=0; i<=30; i++){
@@ -13,10 +13,35 @@ bool isPowerOfTwo(int n){
     return false;
 }
 
+//using bit manipulation on the number
+bool optimisedIsPowerOfTwo(int n){
+    int number_of_set_bits=0,current_bit;
+    while(n!=0 && number_of_set_bits<=1){
+        current_bit=n&1;
+        if(current_bit == 1){
+            number_of_set_bits++;
+        }
+        n=n>>1;
+    }
+
+    if(n==0 && number_of_set_bits==1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 int main()
 {
     int num;
+    cout<<"Enter a number:: ";
     cin>>num;
-    cout<<isPowerOfTwo(num);
+    if(isPowerOfTwo(num)){
+        cout<<num<<" can be represented as 2^x";
+    }
+    else{
+        cout<<num<<" can not be represented as 2^x";
+    }
     return 0;
 }
