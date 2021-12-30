8aae6bc0b93069d49759668830bed6869f0081e3
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2021-12-30 20:35:11 +0530

    added : Documentation for Painters Partition Problem

--- Code_help/20_Painters_Partion_Problem.cpp
+++ Code_help/20_Painters_Partion_Problem.cpp
@@ -5,22 +5,25 @@ bool isPossible(vector<int> arr, int n, int k, int mid){
     int painters = 1;
     int boards = 0;
     for(int i = 0; i<n; i++){
-        if(boards + arr[i] <= mid){
-            boards += arr[i];
+        if(boards + arr[i] <= mid){ // check before allocating 
+            boards += arr[i]; // allocate only if possible 
         }
-        else{
-            painters ++;
-            if(painters > k || arr[i] > mid){
+        else{// if not possible
+            painters ++; // increase count of painters
+            if(painters > k || arr[i] > mid)// and return false if number of painters exceeds k or current element is greater that mid itself 
+            {
                 return false;
             }
-            boards = arr[i];
+            boards = arr[i];// if everything is ok then allocate the current number of boards to next painter
         }
     }
     return true;
 }
 
+// allocates number of boards between k painters to paint all boards in minimum time
 int partitionBoard(vector<int> arr, int n, int k){
     int ans = -1;
+    // search space is 0 ---------- sum of all elements
     int s = 0;
 
     int sum =0;
@@ -32,13 +35,13 @@ int partitionBoard(vector<int> arr, int n, int k){
     int mid = s + (e-s)/2;
     while(s<=e){
         if(isPossible(arr, n, k, mid)){
-            ans = mid;
-            e = mid - 1;
+            ans = mid;// save the possible ans and
+            e = mid - 1; // move to the right of the search space
         }
-        else{
-            s = mid + 1;
+        else{// in case of no solution
+            s = mid + 1; // move to the left of the search space
         }
-        mid = s + (e-s)/2;
+        mid = s + (e-s)/2;// update mid
     }
     return ans;
 }
