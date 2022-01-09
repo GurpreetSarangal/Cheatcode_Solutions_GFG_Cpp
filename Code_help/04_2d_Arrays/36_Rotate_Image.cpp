5b6f3cd20174a6e2c2fb0df0da777baf83e79cd6
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-01-09 21:47:33 +0530

    Added : transpose solution to rotate 2d array problem

--- Code_help/04_2d_Arrays/36_Rotate_Image.cpp
+++ Code_help/04_2d_Arrays/36_Rotate_Image.cpp
@@ -97,6 +97,22 @@ public:
     
 };
 
+ void rotate2(vector<vector<int>>& matrix) {
+        int  n= matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+
+
 int main()
 {
     vector<vector<int>> mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
