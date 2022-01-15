3c768fd9743dcf02f75651d06315d4436eb3a9d7
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-01-15 19:17:56 +0530

    Updated : Solution of Segmented Sieve

--- Code_help/05_maths_for_DSA/41_Segmented_Sieve.cpp
+++ Code_help/05_maths_for_DSA/41_Segmented_Sieve.cpp
@@ -1,107 +1,62 @@
-// C++ program to print print all primes smaller than
-// n using segmented sieve
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-// This functions finds all primes smaller than 'limit'
-// using simple sieve of eratosthenes. It also stores
-// found primes in vector prime[]
-void simpleSieve(int limit, vector<int> &prime)
-{
-    // Create a boolean array "mark[0..n-1]" and initialize
-    // all entries of it as true. A value in mark[p] will
-    // finally be false if 'p' is Not a prime, else true.
-    vector<bool> mark(limit + 1, true);
-
-    for (int p=2; p*p<limit; p++)
-    {
-        // If p is not changed, then it is a prime
-        if (mark[p] == true)
-        {
-            // Update all multiples of p
-            for (int i=p*p; i<limit; i+=p)
-                mark[i] = false;
-        }
-    }
-
-    // Print all prime numbers and store them in prime
-    for (int p=2; p<limit; p++)
-    {
-        if (mark[p] == true)
-        {
-            prime.push_back(p);
-            cout << p << " ";
+void printArray();
+void boolprint(vector<bool>);
+vector<long long> prime;
+void sieve(int n){
+    vector<bool> isPrime(n+1, true);
+    for(long long i=2; i<=n; i++){
+        if(isPrime[i]){
+            prime.push_back(i);
+            for(long long j=i*i; j<=n; j+=i){
+                isPrime[j] = false;
+            }
         }
     }
 }
 
-// Prints all prime numbers smaller than 'n'
-void segmentedSieve(int n)
-{
-    // Compute all primes smaller than or equal
-    // to square root of n using simple sieve
-    int limit = floor(sqrt(n))+1;
-    vector<int> prime; 
-    prime.reserve(limit);
-    simpleSieve(limit, prime); 
-
-    // Divide the range [0..n-1] in different segments
-    // We have chosen segment size as sqrt(n).
-    int low = limit;
-    int high = 2*limit;
-
-    // While all segments of range [0..n-1] are not processed,
-    // process one segment at a time
-    while (low < n)
-    {
-        if (high >= n) 
-           high = n;
-        
-        // To mark primes in current range. A value in mark[i]
-        // will finally be false if 'i-low' is Not a prime,
-        // else true.
-        bool mark[limit+1];
-        memset(mark, true, sizeof(mark));
-
-        // Use the found primes by simpleSieve() to find
-        // primes in current range
-        for (int i = 0; i < prime.size(); i++)
-        {
-            // Find the minimum number in [low..high] that is
-            // a multiple of prime[i] (divisible by prime[i])
-            // For example, if low is 31 and prime[i] is 3,
-            // we start with 33.
-            int loLim = floor(low/prime[i]) * prime[i];
-            if (loLim < low)
-                loLim += prime[i];
-
-            /* Mark multiples of prime[i] in [low..high]:
-                We are marking j - low for j, i.e. each number
-                in range [low, high] is mapped to [0, high-low]
-                so if range is [50, 100] marking 50 corresponds
-                to marking 0, marking 51 corresponds to 1 and
-                so on. In this way we need to allocate space only
-                for range */
-            for (int j=loLim; j<high; j+=prime[i])
-                mark[j-low] = false;
+void segmentSieve(int l, int h){
+    long long sq = floor(sqrt(h));
+    sieve(sq);
+    printArray();
+    vector<bool> isPrime(h-l+1, true);
+    boolprint(isPrime);
+    for(long long p: prime){
+        long long sm = (l/p)*p;
+        cout<<"sm = "<<sm<<endl;
+        cout<<"p = "<<p<<endl;
+        if(sm < l)
+            sm += p;
+        for(long long m = sm*2; m<=h; m+=p){
+            isPrime[m-l] = false;
         }
-
-        // Numbers which are not marked as false are prime
-        for (int i = low; i<high; i++)
-            if (mark[i - low] == true)
-                cout << i << " ";
-
-        // Update low and high for next segment
-        low = low + limit;
-        high = high + limit;
+        boolprint(isPrime);
     }
+    printArray();
+    cout<<"The prime numbers between "<<l<<" and "<<h<<" are:\n";
+    for(long long i=l; i<=h; i++){
+        if(isPrime[i-l] == true)
+            cout<<i<<"  ";
+    }cout<<endl;
 }
 
-// Driver program to test above function
+void printArray(){
+    for(long long i:prime){
+        cout<<i<<"  ";
+    }cout<<endl;
+}
+
+void boolprint(vector<bool> arr){
+    for(int i =0; i<arr.size(); i++){
+        cout<<i+1<<"->"<<arr[i]<<"  ";
+    }cout<<endl;
+}
 int main()
 {
-    int n = 100000;
-    cout << "Primes smaller than " << n << ":n";
-    segmentedSieve(n);
+    int l, h;
+    cout<<"Enter lower and upper limits: \n";
+    cin>>l>>h;
+    printArray();
+    segmentSieve(l,h);
     return 0;
 }
